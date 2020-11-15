5e8360f575f87dca7bc7ffd7b1726813052947f9
Ivan Lee <ilee.code@gmail.com>
2020-11-15 04:36:13 -0800

    Create TweetBox component

---
+++ src/TweetBox.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./TweetBox.css"
+
+
+function TweetBox() {
+  return (
+    <div className="tweetBox">
+      <form>
+        <div className="tweetBox__input">
+
+        </div>
+        <Button>Tweet</Button>
+      </form>
+    </div>
+  )
+}
+
+export default TweetBox
