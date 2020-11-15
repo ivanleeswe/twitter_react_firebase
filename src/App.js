7362f4329fc4072ba87cd6a141a4f92c761077f8
Ivan Lee <ilee.code@gmail.com>
2020-11-15 03:34:01 -0800

    Add feed component and line between components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Sidebar from "./Sidebar";
+import Feed from "./Feed";
 import "./App.css";
 
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <div className="app">
       <Sidebar />
+      <Feed />
     </div>
   );
 }
---
+++ src/Feed.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Feed() {
+  return (
+    <div>
+      asasdadads
+    </div>
+  )
+}
+
+export default Feed;
