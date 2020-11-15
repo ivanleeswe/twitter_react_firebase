544e875bdd47c0d79eb797c1ead22f7412a2ba38
Ivan Lee <ilee.code@gmail.com>
2020-11-15 12:00:41 -0800

    Install TwitterTweetEmbed for the Widget Component

--- src/Widgets.js
+++ src/Widgets.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import "./Widgets.css";
+import {
+  TwitterTimelineEmbed,
+  TwitterShareButton,
+  TwitterTweetEmbed,
+} from "react-twitter-embed";
 
 function Widgets() {
   return (
